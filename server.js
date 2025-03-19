2505a49b41c24c342a6a58f2bf0cb95bcdd92be1
Mahesh <shettydeeksha810@gmail.com>
2025-03-19 14:28:19 +0400

    Backend done!

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
-import ttsRoutes from "./routes/ttsRoutes.js";
+import authRoutes from "./api/routes/authRoutes.js";
+import ttsRoutes from "./api/routes/ttsRoutes.js";
 
 dotenv.config();
 
